29da76d5524138df28baff67d1a4833acb16b4f4
weyn <kyryloulianenko@gmail.com>
2026-09-09 14:27:51 +0200

    added task.html

---
+++ Taskly/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def dashboard():
+    return render_template("dashboard.html")
+
+
+@app.route("/tasks")
+def tasks():
+    return render_template("tasks.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
