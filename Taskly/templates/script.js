b80808e7d12b712bf109fd09b2aa95e3bca7fd28
weyn <kyryloulianenko@gmail.com>
2026-09-09 13:25:56 +0200

    added task creation functionality

--- Taskly/templates/script.js
+++ Taskly/templates/script.js
@@ -1,4 +1,7 @@
 const checkboxes = document.querySelectorAll(".task input[type='checkbox']");
+const input = document.getElementById("taskInput");
+const button = document.getElementById("addButton");
+const taskList = document.getElementById("taskList")
 
 checkboxes.forEach(function(checkbox) {
     checkbox.addEventListener("change", function() {
@@ -6,4 +9,38 @@ checkboxes.forEach(function(checkbox) {
 
         task.classList.toggle("completed");
     });
+});
+
+button.addEventListener("click",
+    function () {
+
+        const taskText = input.value;
+
+        if (taskText === "") {
+            return;
+        }
+
+        // create card
+        const task = document.createElement("div");
+        task.classList.add("task");
+
+        // create checkbox
+        const checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+
+        // create text
+        const text = document.createElement("span");
+        text.textContent = input.value;
+
+        // create delete button
+        const deleteButton = document.createElement("button");
+        deleteButton.textContent = "🗑";
+
+        task.appendChild(checkbox);
+        task.appendChild(text);
+        task.appendChild(deleteButton);
+
+        taskList.appendChild(task); // add on site
+
+        input.value = "";
 });
