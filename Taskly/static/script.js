d5f3e98b397c7c91fbd7652cb147a1334c225438
weyn <kyryloulianenko@gmail.com>
2026-09-10 20:00:43 +0200

    modified html ctructure

--- Taskly/static/script.js
+++ Taskly/static/script.js
@@ -2,51 +2,55 @@ const input = document.getElementById("taskInput");
 const button = document.getElementById("addButton");
 const taskList = document.getElementById("taskList")
 
-button.addEventListener("click",
-    function () {
+if (input && button && taskList) {
 
-        const taskText = input.value;
-
-        if (taskText === "") {
-            return;
-        }
-
-        // create card
-        const task = document.createElement("div");
-        task.classList.add("task");
-
-        // create checkbox
-        const checkbox = document.createElement("input");
-        checkbox.type = "checkbox";
-
-        // create text
-        const text = document.createElement("span");
-        text.textContent = input.value;
-
-        // create delete button
-        const deleteButton = document.createElement("button");
-        deleteButton.textContent = "🗑";
-
-        // checkbox
-        checkbox.addEventListener("change", function () {
-
-            if (checkbox.checked) {
-                text.style.textDecoration = "line-through";
-            } else {
-                text.style.textDecoration = "none";
+    button.addEventListener("click",
+        function () {
+        
+            const taskText = input.value;
+        
+            if (taskText === "") {
+                return;
             }
+        
+            // create card
+            const task = document.createElement("div");
+            task.classList.add("task");
+        
+            // create checkbox
+            const checkbox = document.createElement("input");
+            checkbox.type = "checkbox";
+        
+            // create text
+            const text = document.createElement("span");
+            text.textContent = input.value;
+        
+            // create delete button
+            const deleteButton = document.createElement("button");
+            deleteButton.textContent = "🗑";
+        
+            // checkbox
+            checkbox.addEventListener("change", function () {
+            
+                if (checkbox.checked) {
+                    text.style.textDecoration = "line-through";
+                } else {
+                    text.style.textDecoration = "none";
+                }
+            
+            });
+        
+            deleteButton.addEventListener("click", function () {
+                task.remove();
+            });
+        
+            task.appendChild(checkbox);
+            task.appendChild(text);
+            task.appendChild(deleteButton);
+        
+            taskList.appendChild(task); // add on site
+        
+            input.value = "";
+    });
 
-        });
-
-        deleteButton.addEventListener("click", function () {
-            task.remove();
-        });
-
-        task.appendChild(checkbox);
-        task.appendChild(text);
-        task.appendChild(deleteButton);
-
-        taskList.appendChild(task); // add on site
-
-        input.value = "";
-});
+}
